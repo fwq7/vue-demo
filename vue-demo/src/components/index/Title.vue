41ccf10d7ae1bb6c9ea1b17fd6d037f31e5cf4eb
fwq <1430833756@qq.com>
2021-02-02 19:08:20 +0800

    删除注释

--- vue-demo/src/components/index/Title.vue
+++ vue-demo/src/components/index/Title.vue
@@ -15,55 +15,6 @@ export default {
     titleconverst: Function,
     refresh: Function
   }
-  // data () {
-  //   return {
-  //     titles: [],
-  //     articlestotal: [],
-  //     articlelist:[]
-  //   }
-  // },
-  // computed: {
-  //   titleconverst () {
-  //     return function (title) {
-  //       var temp = title.name
-  //       if (temp === '前端'){
-  //         temp = 'qd'
-  //       }
-  //       if (temp === '数据库'){
-  //         temp = 'db'
-  //       }
-  //       return temp
-  //     }
-  //   }
-  // },
-  // mounted () {
-  //   const  _this = this
-  //   const url = apiUrl.apiUrl+'/article/cagegorys'
-  //   this.$axios.get(url).then(response => {
-  //     this.titles = response.data
-  //   })
-  //   this.refresh(1)
-  // },
-  // methods: {
-  //   refresh (category) {
-  //     const url = apiUrl.apiUrl+'/article/cagegorys/'+category
-  //     const url2 = apiUrl.apiUrl+'/article/cagegorys/'+category+'/1'
-  //     this.$axios.get(url).then(response => {
-  //     this.articlestotal = response.data
-  //     console.log(this.articlestotal)
-  //     //发布消息
-  //     PubSub.publish('count', this.articlestotal.length)
-  //   })
-  //     this.$axios.get(url2).then(response => {
-  //       this.articleslist = response.data
-  //       //发布消息
-  //       console.log(this.articleslist)
-  //       PubSub.publish('articlelist', this.articlelist)
-  //     })
-  //   },
-  //   articlebycategory (category) {
-  //   }
-  // }
 }
 </script>
 <style>
